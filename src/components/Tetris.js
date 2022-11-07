29549a3c38368c62c04b9d667d3e74df661eb1c7
mario <mariolopezabc@gmail.com>
2022-11-07 13:33:57 -0600

    Starting piece now appears

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -8,8 +8,14 @@ import { usePlayer } from '../hooks/usePlayer';
 
 const Tetris = ({rows, columns, setGameOver}) => {
   const [gameStats, addLinesCleared] = useGameStats();
-  const [board, setBoard] = useBoard({ rows, columns});  
   const [player, setPlayer, resetPlayer] = usePlayer();
+  const [board, setBoard] = useBoard({ 
+    rows,
+    columns,
+    player,
+    resetPlayer,
+    addLinesCleared
+  }); 
 
   return (
     <div className="Tetris">
