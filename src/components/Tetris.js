7a68ea4d27817221a70a44ea0271683b06eba897
mario <mariolopezabc@gmail.com>
2022-11-06 22:32:28 -0600

    Added game stats to the left of board

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,12 +1,18 @@
 import "./Tetris.css";
 import Board from "../components/Board";
 import {useBoard } from "../hooks/useBoard";
+import GameStats from "./GameStats";
+import { useGameStats } from '../hooks/useGameStats';
 
 const Tetris = ({rows, columns, setGameOver}) => {
+  const [gameStats, addLinesCleared] = useGameStats();
   const [board, setBoard] = useBoard({ rows, columns});  
 
   return (
-    <Board board={board}/>
+    <div className="Tetris">
+      <Board board={board}/>
+      <GameStats gameStats={gameStats} />
+    </div>
   );
 }
 
